80b8dc8888f0fcf63f400db4d093e4f4bd83059c
Steven Cardenas <283815984+stevencardenas615@users.noreply.github.com>
2026-09-06 20:02:18 -0400

    Finished the summary command, pulled the repetetive code into a query builder that can be reused

--- portfolio/finance_cli.py
+++ portfolio/finance_cli.py
@@ -23,6 +23,19 @@ def db_connect(database_file):
 def db_disconnect(connector):
     connector.commit()
     connector.close()
+
+def search_builder(choice: AllowedCategory | None = None, start: datetime | None = None, end: datetime | None = None):
+    
+    filters = [
+        ("category = ?", choice.value if choice else None, choice is not None),
+        ("date >= ?", start.strftime("%Y-%m-%d") if start else None, start is not None),
+        ("date <= ?", end.strftime("%Y-%m-%d") if end else None, end is not None)
+        ]
+
+    where_prompts = [sql for sql, val, active in filters if active]
+    parameters = [val for sql, val, active in filters if active]
+
+    return where_prompts, parameters
    
 @app.command()
 def add(amount: float, category: AllowedCategory, date: datetime | None = None, note: str = ""):
@@ -40,17 +53,9 @@ def add(amount: float, category: AllowedCategory, date: datetime | None = None, 
 def pull(choice: AllowedCategory | None = None, start: datetime | None = None, end: datetime | None = None):
     conn, cursor = db_connect("transactions.db")
 
-    filters = [
-        ("category = ?", choice.value if choice else None, choice is not None),
-        ("date >= ?", start.strftime("%Y-%m-%d") if start else None, start is not None),
-        ("date <= ?", end.strftime("%Y-%m-%d") if end else None, end is not None)
-        ]
-
-    where_prompts = [sql for sql, val, active in filters if active]
-    parameters = [val for sql, val, active in filters if active]
+    where_prompts, parameters = search_builder(choice, start, end)
 
     query = "SELECT * FROM history"
-
     if where_prompts:
         query += " WHERE " + " AND ".join(where_prompts)
     query += " ORDER BY date DESC"
@@ -69,6 +74,31 @@ def pull(choice: AllowedCategory | None = None, start: datetime | None = None, e
 
     db_disconnect(conn)
 
+@app.command()
+def summary(choice: AllowedCategory | None = None, start: datetime | None = None, end: datetime | None = None):
+    conn, cursor = db_connect("transactions.db")
+
+    where_prompts, parameters = search_builder(choice, start, end)
+
+    query = "SELECT category, SUM(amount) FROM history"
+
+    if where_prompts:
+        query += " WHERE " + " AND ".join(where_prompts)
+
+    query += " GROUP BY category ORDER BY SUM(amount) DESC"
+
+    report = cursor.execute(query, parameters).fetchall()
+
+    if not report:
+        print("No Transactions")
+    else:
+        output = [f"{row[0].upper()}: ${row[1]:.2f}" for row in report]
+        total = [row[1] for row in report]
+        for line in output:
+            print(line)
+        print(f"TOTAL: ${sum(total):.2f}")
+
+    db_disconnect(conn)
         
 if __name__ == "__main__":
     app()
